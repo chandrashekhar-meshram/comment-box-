2069ece8156a13ed5a193cd81b7ad890cb9288fa
chandrashekhar-meshram <shekharonlinework@gmail.com>
2022-11-11 20:08:42 +0530

    new file added

--- final.js
+++ final.js
@@ -1,149 +1,156 @@
-const userInput = document.getElementById('textArea');
-const inputBtn = document.getElementById('input-btn');
-const displayArea = document.getElementById('displayArea');
 
-//1. function to get the local data
-// window.onload = function setTemplate() {
-//     displayArea.innerHTML = localStorage.getItem('template');
-// }
 
-// //2. function to save data in local storage
-// const saveToLocal = () => {
-//     localStorage.setItem('template', displayArea.innerHTML);
-// }
+//create commentContainer variable 
+const commentContainer = document.getElementById('allComments');
 
-const likeImg = `<i id=like-img class="fa-solid fa-thumbs-up" ></i><br>`;
-// const deleteImg = `<i class="fa-solid fa-trash"></i>`;
-const replyBtn = `<i class="fa-sharp fa-solid fa-comment-dots"></i>`;
+// part 1   => function to get the local data
+window.onload = function setTemplate() {
+    // document.getElementById('allComments').innerHTML = localStorage.getItem('template');
+    commentContainer.innerHTML = localStorage.getItem('template');
+};
 
-let userComment = '';
-let commentData = [];
 
-// store input value
-userInput.addEventListener('change', (e) => {
-    userComment = e.target.value
-    console.log(userComment);
-})
+// part 2  => function to save data in local storage
+function saveToLocal() {
+    // localStorage.setItem('template', document.getElementById('allComments').innerHTML);
+    localStorage.setItem('template', commentContainer.innerHTML);
+}
 
-const addComment = () => {
-    //temp object to store data
-    let commentObj = {
-        id: commentData.length + 1,
-        like: 0,
-        reply: []
-    } 
+// part 5  - https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_html_hasclass
+// checking class name present or not ..if class present return true otherwise false
+function hasClass(elem, className) {
+    // console.log(elem.className.split(' ').indexOf(className)); // output: true or false
+    return elem.className.split(' ').indexOf(className) > -1;
+}
 
-    commentData.push(commentObj);
 
-    // let commentId = commentData; Doubt   // comment_1
+// part 3  => Add comments dynamically
+//reply, like ,delete buttons  
+function addComment(ev) {
+    let commentText, wrapDiv;
+    const textBox = document.createElement('div');
 
-    let html = `
-        <div id=comment_${commentObj.id} class=comments-div>
-           <p class=comments>${userComment}<p>   
+    // create button structure for reply
+    const replyButton = document.createElement('button');
+    replyButton.className = 'reply';
+    replyButton.innerHTML = 'Reply';
 
-           <button id=like_${commentObj.id} class=btn>
-            ${likeImg} Like
-           </button>
+    // for like
+    const likeButton = document.createElement('button');
+    likeButton.className = 'likeComment';
+    likeButton.innerHTML = `Like`;
+   
 
-           <button id=reply_${commentObj.id} class="btn reply-btn">
-            ${replyBtn}<br>Reply
-           </button>
+    // for delete
+    const deleteButton = document.createElement('button');   
+    deleteButton.className = 'deleteComment';
+    deleteButton.innerHTML = `Delete`;
+    
 
-           <button id=delete_${commentObj.id} class='btn delete-btn'>
-            Delete
-           </button>
-        </div> `;
-                
-    if(userComment.length > 0){
-        displayArea.innerHTML += html;
+    // add comment from main container with textBox, replyButton, likeButton, deleteButton.
+    if (hasClass(ev.target.parentElement, 'container')) {
+        // console.log(ev.target.parentElement);
+        const wrapDiv = document.createElement('div');
+        wrapDiv.className = 'wrapper';
+        wrapDiv.style.marginLeft = 0;
+        // storing textarea value of main textarea
+        commentText = document.getElementById('comment').value;
+        // after click on add button empty main textarea
+        document.getElementById('comment').value = '';
+
+        //adding text from main commentBox
+        if (commentText.length > 0) {
+            textBox.innerHTML = commentText;
+            textBox.style.backgroundColor = "cornflowerblue";
+            //append all 
+            wrapDiv.append(textBox, replyButton, likeButton, deleteButton);
+            commentContainer.appendChild(wrapDiv);
+        }
+
     }
-    console.log(userComment);
+    else {
+        // this is for child comment add
+        // it will work after reply comment on click add button
+        wrapDiv = ev.target.parentElement;
+        // console.log(ev.target.parentElement);
+        commentText = ev.target.parentElement.firstElementChild.value;
 
-    //userComment = ""; // doubt - will be for reply
-    console.log(userComment);
+        console.log('commentText', commentText);
 
-    userInput.value = ""; // empty main input box
-
+        if (commentText.length > 0) {
+            textBox.innerHTML = commentText;
+            textBox.style.backgroundColor = "tomato";
+            // remove textarea after add reply on child
+            wrapDiv.innerHTML = '';
+            // add all button same as like parenth comment
+            wrapDiv.append(textBox, replyButton, likeButton, deleteButton);
+        }
+    }
     saveToLocal();
 }
 
-//calling addComment on button click
-inputBtn.addEventListener('click', addComment);
+// // part 4
+// adding event listner onbutton click to call addComment
+document.getElementById('addComments').addEventListener('click', function (ev) {
+    // call addComment function
+    addComment(ev);
+});
+// // Add comments dynamically  ends here
 
-// adding single event to perent using Event Deligation
-displayArea.addEventListener('click', (e) => {
-    console.log(e.target);
+// //https://javascript.info/localstorage
 
-    if(e.target.tagName === "BUTTON") {
-        // getting index
-        const index = e.target.id.split("_")[1]; // after split we will get =>  ['like', 1] and from index 1 we will get id number like 1, 2etc
 
-        console.log(index); //1,2 etc
 
-        // https://www.w3schools.com/jsref/prop_node_textcontent.asp  
-        const id = 'comment_' + index;
-        const commentDiv = document.querySelector(`#${id}`);
-        // #comment_1
 
-        //document.querySelector("#inputbtn")
+// // part 6
+document.getElementById('allComments').addEventListener('click', function (e) {
+    // creating HTML for reply
+    if (hasClass(e.target, 'reply')) {
+        const parentDiv = e.target.parentElement;
+        const wrapDiv = document.createElement('div');
+        wrapDiv.style.marginLeft = (Number.parseInt(parentDiv.style.marginLeft) + 15).toString() + 'px';
+        wrapDiv.className = 'wrapper';
+        const textArea = document.createElement('textarea');
+        textArea.style.marginRight = '20px';
 
-      console.log(commentDiv);
-       
-      if(e.target.textContent.includes('Like')) {
-            commentData[index-1].like += 1;
+        // ADD button create for reply box on click
+        const addButton = document.createElement('button');
+        addButton.className = 'addReply';
+        addButton.innerHTML = 'Add';
 
-            // e.target.textContent = commentData[index-1].like + "Like"; // we need to add Like otherwise it will work only 1 time
+        // cancel button  create for reply box on click
+        const cancelButton = document.createElement('button');
+        cancelButton.innerHTML = 'Cancel';
+        cancelButton.className = 'cancelReply';
 
-            e.target.innerHTML =  `${likeImg} ${commentData[index-1].like}- Like`; // we need to add Like otherwise it will work only 1 time
+        wrapDiv.append(textArea, addButton, cancelButton);
+        parentDiv.appendChild(wrapDiv);
 
-            console.log(e.target.textContent);
+    }
 
-            saveToLocal();
-        }
-        else if(e.target.textContent === 'Delete') {
-            console.log(e.target.textContent);
-            commentDiv.remove();
-            //saveToLocal();
-        }   
-        else if(e.target.textContent.includes('Reply')) {
-            const replyIndex =  commentData[index-1].reply.length + 1;
-            console.log('replyIndex = ', replyIndex); //1, 2
+    // adding all the html data from addComment function for reply
 
-            const replyBox = `
-                <div id=replyContainer_${index}> 
-                    <textarea type="text" id=replyinput_${index}_${replyIndex} class=input-area></textarea>
-                    <br/>
-                    <button id=replyinput_${index}_${replyIndex} class=btn>Save</button>
-                    <button id=replyinput_${index}_${replyIndex} class=btn>Cancel</button>                         
-                </div>`;  
+    else if (hasClass(e.target, 'addReply')) {
+        addComment(e);
+    }
 
-            commentDiv.innerHTML += replyBox; // #comment_1
-            console.log(commentDiv);
-        }
-        else if(e.target.textContent === 'Save') {
-            const replyIndex = e.target.id.split('_')[2]; //id = comment_1_1
-            let val = '';
+    // adding like on button click
 
-            if(replyIndex) {
-                //store value of reply input box
-                val = document.querySelector(`#replyinput_${index}_${replyIndex}`).value; 
-            }
+    else if (hasClass(e.target, 'likeComment')) {
+        const likeBtnValue = e.target.innerHTML;
+        e.target.innerHTML = likeBtnValue !== 'Like' ? Number.parseInt(likeBtnValue) + 1 + " Like" : 1 + " Like";
+        saveToLocal();
+    }
+    // cancel reply on button click
 
-            if(val != '') {
-                (e.target.parentElement.parentElement).innerHTML += `
-                 <div class=all-reply-input>${val}</div>`;
-                 
-                 // remove reply comment box
-                 document.querySelector(`#${e.target.parentElement.id}`).remove();
+    else if (hasClass(e.target, 'cancelReply')) {
+        e.target.parentElement.innerHTML = '';
+        saveToLocal();
+    }
 
-                 commentData[index-1].reply.push(val);
-
-                 saveToLocal();
-            }
-            console.log(commentData)
-        } 
-        else if(e.target.textContent === 'Cancel') {
-              document.querySelector(`#${e.target.parentElement.id}`).remove();
-        }  
-    } 
-})
+    // delete entire  comment chain
+    else if (hasClass(e.target, 'deleteComment')) {
+        e.target.parentElement.remove();
+        saveToLocal();
+    }
+});
